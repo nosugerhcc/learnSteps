de5d6d632942b18458464ec1ea28d307a0840cc6
hanchenchen <hanchenchen03@kuaishou.com>
2022-01-31 16:55:04 +0800

    aop

--- src/main/java/com/hcc/learnsteps/leetcode/hot100/Solution4.java
+++ src/main/java/com/hcc/learnsteps/leetcode/hot100/Solution4.java
@@ -1,17 +1,58 @@
 package com.hcc.learnsteps.leetcode.hot100;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 /**
  * @author hanchenchen <hanchenchen@kuaishou.com>
  * Created on 2021-12-25
  */
 public class Solution4 {
-    public static void main(String[] args) {
-        String s = "hcc";
-        Set<String> collect = Arrays.stream(s.split(",")).collect(Collectors.toSet());
-        System.out.println(collect);
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+            } else {
+                return nums2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+            } else {
+                return nums1[m / 2];
+            }
+
+        }
+
+        int[] nums = new int[m + n];
+        int count = 0;
+        int i = 0, j = 0;
+        while (count != (m + n)) {
+            if (i == m) {
+                while (j != n) {
+                    nums[count++] = nums2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i != m) {
+                    nums[count++] = nums1[i++];
+                }
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                nums[count++] = nums1[i++];
+            } else {
+                nums[count++] = nums2[j++];
+            }
+        }
+
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
     }
+
 }
